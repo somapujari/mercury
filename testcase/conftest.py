8c756eac71cef749dd2358b4db28f28783dda1c6
soma <somapujari0@gmail.com>
2023-08-07 11:38:57 +0530

    conftest file modified

--- testcase/conftest.py
+++ testcase/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 
 def pytest_addoption(parser):
@@ -10,9 +11,12 @@ def pytest_addoption(parser):
 def setup(request):
     browser_name = request.config.getoption("--browser")
     if browser_name == 'chrome':
-        driver = webdriver.Chrome()
+        option = webdriver.ChromeOptions()
+        option.binary_location = r'C:\Users\Dell\AppData\Local\Google\Chrome\Application\chrome.exe'
+        # path = ''
+        # service = Service(path)
+        # service.start()
+        driver = webdriver.Chrome(options=option)
     else:
         driver = webdriver.Firefox()
-
-    yield driver
-    driver.quit()
+    return driver
